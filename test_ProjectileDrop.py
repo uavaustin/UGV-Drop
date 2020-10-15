572ac51f3114d359ab6380d3823b4e498df69b69
Jordan Burton <jordane.burton@gmail.com>
2020-10-14 22:10:16 -0500

    bout to push

--- test_ProjectileDrop.py
+++ test_ProjectileDrop.py
@@ -20,12 +20,15 @@ class TestMethods(unittest.TestCase):
         y = 6.5
         z = 21.4
         testPoint = point(x, y, z)
+
         self.assertEqual(testPoint.getX(), x)
         self.assertEqual(testPoint.getY(), y)
         self.assertEqual(testPoint.getZ(), z)
+
         testPoint.setX(13)
         testPoint.setY(2.0)
         testPoint.setZ(29.8888)
+
         self.assertEqual(testPoint.getX(), 13)
         self.assertEqual(testPoint.getY(), 2)
         self.assertEqual(testPoint.getZ(), 29.8888)
@@ -36,11 +39,29 @@ class TestMethods(unittest.TestCase):
         yv = -5.4321
         zv = -9.426
         testVector = vector(xv, yv, zv)
+
         self.assertEqual(testVector.getX(), xv)
         self.assertEqual(testVector.getY(), yv)
         self.assertEqual(testVector.getZ(), zv)
 
 #testing all dropCalculations methods
+    def testDropCalcMethods(self):
+        velVec = vector(10, 2, -3)
+        accelVec = vector(-1, 20, 3)
+        projLoc = point(12, 5.5, 0.6)
+        targetLoc = point(0, 0, 0)
+        coeffDrag = 0.1
+        chuteAndUGVMass = 20
+        chuteArea = 70
+        chuteDeployTime = 2
+
+        testDropCalc = dropCalculations(velVec,accelVec,projLoc,targetLoc,coeffDrag,chuteAndUGVMass,chuteArea,chuteDeployTime)
+
+        testDropCalc.updateLocation(1,2,3):
+        self.assertEqual(testDropCalc.projX, 1)
+        self.assertEqual(testDropCalc.projY, 2)
+        self.assertEqual(testDropCalc.projZ, 3)
+#this is not done lol
 
 
 
