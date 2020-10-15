b39d123ddff3622237791dcba959c1b0abfb131e
sydneyrabara <sydneyrabara@yahoo.com>
2020-10-15 16:56:25 -0500

    started unit test

---
+++ unit_test_sr.py
@@ -0,0 +1,17 @@
+"""
+unit testing UGV drop projectile
+
+author : sydney rabara
+
+assume:
+maxY = 30.48
+No negative components for points class
+The target is origin
+
+
+
+"""
+
+import unittest
+
+from Projectile import *
