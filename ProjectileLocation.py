da6f269f6a90df6c8d05c06872083a6c504149ed
rishthak <rishit.thakkar@utexas.edu>
2020-10-15 18:45:23 -0500

    newly added file

---
+++ ProjectileLocation.py
@@ -0,0 +1,5 @@
+"""
+The ProjectileLocation translates the the location of a projectile in Latitude and Longitude into a coordinate system.
+This class should be run in tandem with the ProjectileDrop file.
+
+"""
