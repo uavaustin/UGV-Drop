0809520727f40dd2891cc20526640186353c90e6
Jordan Burton <jordane.burton@gmail.com>
2020-10-21 01:13:03 -0500

    created unit test file

---
+++ test_ProjectileLocation.py
@@ -0,0 +1,13 @@
+"""
+Unit tests for ProjectileLocation
+
+@author: Jordan
+
+assumptions:
+    points and vectors can have negative components
+    max height is like 30 meters(i think)
+
+"""
+
+import unittest
+from ProjectileLocation import *
