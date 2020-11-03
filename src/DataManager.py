4ce7c362b4b6596dace69f643369f319e3b483c0
rishthak <rishit.thakkar@utexas.edu>
2020-11-03 00:21:21 -0600

    thorough restructuring continued

---
+++ src/DataManager.py
@@ -0,0 +1,8 @@
+"""
+The DataManager class handles the data management for the dropCalculator. Specifically, it can produces various graphs
+based on the data output by the dropCalculator.
+
+@author rishthak
+"""
+
+#TBD
