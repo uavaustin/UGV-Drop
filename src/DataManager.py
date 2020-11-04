1cda99132965ac993dce1cc8d9302ecd646ba8c8
rishthak <rishit.thakkar@utexas.edu>
2020-11-04 17:39:45 -0600

    creating data manager class

--- src/DataManager.py
+++ src/DataManager.py
@@ -12,3 +12,39 @@ import numpy
 from DropCalculator import *
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+
+class manageDropData:
+    def __init__(self,
+                xArray: [],
+                yArray: [],
+                zArray: [],
+                zVelArray: [],
+                zAccArray: [],
+                tArray: [])
+
+        #Define Arrays
+        self.__xArr = xArray
+        self.__yArr = yArray
+        self.__zArr = zArray
+        self.__zVelArr = zVelArray
+        self.__zAccArr = zAccArray
+        self.__tArr = tArray
+
+    def buildAltChart(self):
+        plt.figure(1)
+        plt.scatter(tArray, zArray)
+        plt.xlabel("Time")
+        plt.ylabel("Altitude")
+
+    def buildVelChart(self):
+        plt.figure(2)
+        plt.scatter(tArray, zArray)
+        plt.xlabel("Time")
+        plt.ylabel("Altitude")
+
+    def buildVelChart(self):
+        plt.figure(2)
+        plt.scatter(tArray, zArray)
+        plt.xlabel("Time")
+        plt.ylabel("Altitude")
+
